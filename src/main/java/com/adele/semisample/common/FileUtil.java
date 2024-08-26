e6f22912567bcc78986f8de4f15a0ad3453c30fc
adele <akak4456@naver.com>
2024-08-26 16:06:42 +0900

    [CNG] 주석 추가함

--- src/main/java/com/adele/semisample/common/FileUtil.java
+++ src/main/java/com/adele/semisample/common/FileUtil.java
@@ -11,6 +11,13 @@ import java.util.UUID;
 import static com.adele.semisample.common.FileConfig.REAL_PATH;
 
 public class FileUtil {
+    /**
+     * 파일을 업로드하고 업로드한 파일 정보를 DB 에 저장합니다.
+     * @param files: upload 할 파일 list
+     * @param converter: multipart file 을 db 에 저장할 때 필요한 converter
+     * @return int: db에 저장된 row 개수
+     * @throws IOException: 파일 저장에 오류가 발생할 때
+     */
     public static int  uploadFiles(List<MultipartFile> files, MultipartFileToSqlConverter converter) throws IOException {
         int result = 0;
         for (MultipartFile file : files) {
